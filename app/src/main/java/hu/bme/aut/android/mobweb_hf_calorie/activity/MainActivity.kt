609340b340d1eb5d903e354fd8182315708e50ab
12babos12 <12babos12@gmail.com>
2021-05-14 22:11:27 +0200

    Testing Actions trigger

--- app/src/main/java/hu/bme/aut/android/mobweb_hf_calorie/activity/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/android/mobweb_hf_calorie/activity/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
         btnList.setOnClickListener {
             val intent = Intent(this, ListActivity::class.java)
             startActivity(intent)
